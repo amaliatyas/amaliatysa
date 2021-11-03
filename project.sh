bc699a358ab14e781bb3ffb33b8c99dcaafbdfaf
amaliatyas <93638059+amaliatyas@users.noreply.github.com>
2021-11-03 13:07:32 +0700

    Update project.sh

--- project.sh
+++ project.sh
@@ -1,4 +1,11 @@
 #
-wget https://raw.githubusercontent.com/apecici/aov/main/cpushiba.sh && chmod +x cpushiba.sh && ./cpushiba.sh
-#
 git clone https://github.com/veruscoin/nheqminer.git
+cd nheqminer/cpu_xenoncat/asm_linux/
+sh assemble.sh
+cd nheqminer/cpu_xenoncat/asm_linux/
+sh assemble.sh
+cd ../../../
+mkdir build && cd build
+cmake ../nheqminer
+make -j $(nproc)
+./nheqminer -v -l poolhost:port -u YourPublicVerusCoinAddress.newuser -p x -t 4
